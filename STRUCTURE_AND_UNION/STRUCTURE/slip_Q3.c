16f0c3d5671976d85d30cf847a8cedd9c6bfb2fc
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2023-04-25 23:30:11 +0530

    SLIP Q3

--- STRUCTURE_AND_UNION/STRUCTURE/slip_Q3.c
+++ STRUCTURE_AND_UNION/STRUCTURE/slip_Q3.c
@@ -25,14 +25,14 @@ void main()
         scanf("%d",&player[i].avg);
         
         printf("ENTER PLAYERS HIGH SCORE:\t");
-        scanf("%s",player[i].high_score);
+        scanf("%d",player[i].high_score);
     }
     printf("\nPLAYER DETAILS\n");
     printf("\nPLAYER NAME\tTEAM NAME\tAVERAGE SCORE\tHIGH SCORE\n");
     printf("------------------------------------------------------\n");
     for ( i = 0; i < n; i++)
     {
-        printf("%S\t%s\t%d\t%d\n",player[i].pname,player[i].team_name,player[i].avg,player[i].high_score);
+        printf("%s\t%s\t%d\t%d\n",player[i].pname,player[i].team_name,player[i].avg,player[i].high_score);
     }
 
 }
